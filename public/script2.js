bfb38a3ad0fa36fd9ed414e2b77e050bc0f1c3a0
Kaitlyn Vesterby <kvesterb@terpmail.umd.edu>
2021-03-05 17:10:28 -0500

    working js and api

---
+++ public/script2.js
@@ -0,0 +1,34 @@
+async function windowActions() {
+    const form = document.querySelector('.userform');
+    const search = document.querySelector('#zip');
+
+    const request = await fetch('/api');
+
+    form.addEventListener('submit', async(event) => {
+        event.preventDefault();
+        console.log('submit fired');
+        const display = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase());
+        
+    });
+
+    search.addEventListener('input', (event) => {
+        console.log('input', event.target.value);
+
+    });
+}
+
+window.onload = windowActions;
+
+
+
+//HOW TO WRITE POST REQUEST
+// const request = await fetch('/api', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type':'application/json'
+//     },
+//     body: JSON.stringify({data: search.value})
+// });
+// const data =  await request.json();
+
+// console.table(data.data)
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// These are our required libraries to make the server work.
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
+// // These are our required libraries to make the server work.
+// /* eslint-disable no-console */
+// /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
@@ -20,22 +20,118 @@ app.use((req, res, next) => {
   next();
 });
 
-
-app.route('/api')
-  .get((req, res) => {
+app.route('/api') // endpoint you are using; each route only gets 1 instance of each verb
+  .get(async (req, res) => { // async lets us avoid using then chains
+    // first objects that are passed on a request from express is request and response
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
-  })
-  .post(async (req, res) => {
-    console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-
+    // fetch is easiest way to talk about external delayed requests to other apis and it works the same way on server and browser
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    // getting contents of fetch request and then sending a response to the frontend
     const json = await data.json();
     console.log('data from fetch', json);
+    // json is data type that can be used to communicate info
     res.json(json);
-  });
+  })
+  .post(async (req, res) => { // all database methods are async
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log('Now send something back to your client');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json({data:json});
 
-app.listen(port, () => {
+    // res.json({data: dataToSendToFrontEnd});
+  });
+  app.listen(port, async () => {
   console.log(`Example app listening on port ${port}!`);
 });
+// // app.route('/api')
+// //   .get(async(req, res) => {
+// //     console.log('GET request detected');
+// //     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+// //     const json = await data.json();
+// //     console.log('data from fetch', json);
+// //     res.json(json);
+// //   })
+// //   .post(async (req, res) => {
+// //     console.log('POST request detected');
+// //     console.log('Form data in res.body', req.body);
+
+// //     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+// //     const json = await data.json();
+// //     res.json({data: json});
+// //   });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
+
+/* eslint-disable max-len */
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+// import path from 'path';
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import fetch from 'node-fetch';
+// import reload from 'live-reload';
+// import connectReload from 'connect-livereload';
+
+
+// dotenv.config();
+
+
+// const __dirname = path.resolve(); // computer figures out where all my stuff is
+// const app = express(); // turn on express
+// const port = process.env.PORT || 3000;
+// const staticFolder = 'public'; // where you store your pages you want the internet to see
+
+// // Add some auto-reloading to our server
+// const liveReloadServer = reload.createServer();
+// liveReloadServer.watch(path.join(__dirname, staticFolder)); // this is why my webpages reload when I turn my server on
+
+// // Configure express
+// app.use(connectReload()); // reloading our server when we change files in it so that we can see them on front end 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json()); // why i can send json back from my server when i edit my route
+// app.use(express.static(staticFolder)); 
+
+// app.use((req, res, next) => { // let people connect to this server
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+// app.route('/api') // endpoint you are using; each route only gets 1 instance of each verb
+//   .get(async (req, res) => { // async lets us avoid using then chains
+//     // first objects that are passed on a request from express is request and response
+//     console.log('GET request detected');
+//     // fetch is easiest way to talk about external delayed requests to other apis and it works the same way on server and browser
+//     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+//     // getting contents of fetch request and then sending a response to the frontend
+//     const json = await data.json();
+//     console.log('data from fetch', json);
+//     // json is data type that can be used to communicate info
+//     res.json(json);
+//   })
+//   .post(async (req, res) => { // all database methods are async
+//     console.log('POST request detected');
+//     console.log('Form data in res.body', req.body);
+//     console.log('Now send something back to your client');
+//     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+//     const json = await data.json();
+//     res.json({data:json});
+
+//     // res.json({data: dataToSendToFrontEnd});
+//   });
+
+// app.listen(port, async () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
+
+// liveReloadServer.server.once('connection', () => {
+//   setTimeout(() => {
+//     liveReloadServer.refresh('/');
+//   }, 100);
+// });
+

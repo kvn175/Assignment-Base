34f06c5abf02f8be7673fafdba201f86cb4c586a
Kaitlyn Vesterby <kvesterb@terpmail.umd.edu>
2021-03-05 17:20:37 -0500

    some more changes

--- public/example.js
+++ public/example.js
@@ -1,13 +1,13 @@
 async function windowActions() {
     const form = document.querySelector('.userform');
-    const search = document.querySelector('#zip');
+    const search = document.querySelector('#category');
 
     const request = await fetch('/api');
 
     form.addEventListener('submit', async(event) => {
         event.preventDefault();
         console.log('submit fired');
-        const display = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase());
+        const display = data.filter((record) => record.category.toUpperCase() === search.value.toUpperCase());
         
     });
 

1f508bf5974633ce3f761cb76bd7adb82710f6a4
Kaitlyn Vesterby <kvesterb@terpmail.umd.edu>
2021-03-05 21:55:04 -0500

    CSS and Bulma

--- public/script.js
+++ public/script.js
@@ -14,26 +14,58 @@ const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json
             })
         }
             function displayMatches() {
+                    
                 const matchArray = findMatches(this.value, restaurants);
                 const html = matchArray.map(place => {
                     const regex = new RegExp(this.value, 'gi');
                     const categoryName = place.category.replace(regex, 
                     `<span class='h1'>${this.value}</span>`);
                     return `
+                    <div class = 'boxes'>
                 <li>
                     <span class="name">${place.name}</span>
-                    <span class="category">${place.address_line_1}</span>
-                    <span class="category">${place.city}</span>
-                    <span class="category">${place.state}</span>
-                    <span class="category">${place.zip}</span>
+                    
+                </li>
+                <li>
                     <span class="category">${place.category}</span>
                 </li>
+                <li> 
+                    <address class = italicize>
+                    <span class="category">${place.address_line_1}</span>
+                    </address>
+                </li>
+                <li>
+                    <address class = italicize>
+                    <span class="category">${place.city}</span>
+                    </address>
+                </li>
+                <li>
+                    <address class = italicize>
+                    <span class="category">${place.state}</span>
+                    </address>
+                </li>
+                <li>
+                    <address class = italicize>
+                    <span class="category">${place.zip}</span>
+                    </address>
+                </li>
+                </div>
+                
+                
+                
+                
                 `;
             }).join('');
             suggestions.innerHTML = html;
+            let value = document.getElementById('#search').value;
+            if (value.length > 0){
+                {searchInput.hide()}
+        
         }
         const searchInput = document.querySelector('.search');
         const suggestions = document.querySelector('.suggestions');
 
         searchInput.addEventListener('change', displayMatches);
         searchInput.addEventListener('keyup', displayMatches);
+
+        searchInput.removeEventListener('click', displayMatches);
